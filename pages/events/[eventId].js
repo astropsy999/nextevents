8a1f0fb6f28e5452ebee75d8611e1925e28003c1
astropsy999 <bareust@gmail.com>
2022-08-02 09:40:32 +0300

    added header and layouts

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,35 @@
-export default function EventDetailPage () {
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import { getEventById } from "../../dummy-data";
+import  EventLogistics from "../../components/event-detail/event-logistics";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventContent from "../../components/event-detail/event-content";
+ 
+function EventDetailPage () {
+
+    const router = useRouter()
+    const eventId = router.query.eventId
+    console.log('eventId: ', eventId);
+
+    const event = getEventById(eventId)
+    console.log('event: ', event);
+
+    if(!event) {
+        return <p>No event found!</p>
+    }
+
     return (
-        <div>
-            <h1>Event Detail Page</h1>
-        </div>
+        <Fragment>
+            <EventSummary title={event.title}/>
+            <EventLogistics 
+            date={event.date} 
+            address={event.location} 
+            image={event.image} 
+            imageAlt={event.title}
+            />
+            <EventContent><p>{event.description}</p></EventContent>
+        </Fragment>
     )
 }
+
+export default EventDetailPage;
